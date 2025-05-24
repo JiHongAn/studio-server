8894f779ebbad8ce26fe34553e311ac082f036d2
JihongAn <hongsam0102@gmail.com>
2025-05-24 21:09:44 +0900

    feat: Auth controller, service 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { AuthController } from './controllers/auth.controller';
+import { AuthService } from './services/auth.service';
 
 @Module({
   imports: [PassportModule],
-  providers: [JwtStrategy],
+  providers: [JwtStrategy, AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
